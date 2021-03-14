52554a835e81d3efdea453fe8c79afba9854f636
sonukumarraj007 <sonukumarraj007@gmail.com>
2021-03-14 22:47:13 +0530

    file-added

---
+++ Machin Learing.py
@@ -0,0 +1,88 @@
+
+
+1. Linear Regression
+
+2. Logistic Regression
+
+3. Clustering
+
+------------------------------
+
+1.1 Simple Linear Regression
+
+1.2 Multiple Linear Regression
+
+1.3 Backward Elimination 
+
+1.4 Polynomial Linear Regression
+
+1.5 Decision Tree 
+
+1.6 Random Forest 
+
+1.7 Logistic Regression
+
+1.8 KNN Algorithm 
+
+1.9 Random Forest
+
+1.10. Ridge and Lasso Regression
+
+1.11. Root Mean Square Error MSE vs RMSE
+
+1.11. Training and Testing Data
+
+1.12. Gradient Descent and Cost Function
+
+1.13. Dummy Variables & One Hot Encoding
+
+1.14. K Fold Cross Validation
+
+1.15. K Means Clustering
+
+1.16. Save & Load Machine Learning Model using Pickle & Joblib
+
+1.17. Naive Bayes Classifier Algorithm Theorem
+
+1.18. Elastic Net Regression Explained
+
+1.19. Bayes Theorem
+
+1.20.
+
+1.21.
+
+1.22.
+
+1.23.
+
+1.24.
+
+1.25.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
